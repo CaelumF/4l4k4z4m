1912476ead645c6a33004a1f339ea53ab3104672
caelumF <caelum119@gmail.com>
2017-12-08 23:09:27 +0000

    Added Docker client capability to control host daemon

--- dogberry/src/main/kotlin/main.kt
+++ dogberry/src/main/kotlin/main.kt
@@ -2,6 +2,7 @@ import com.spotify.docker.client.DefaultDockerClient
 import com.spotify.docker.client.messages.ContainerConfig
 import com.spotify.docker.client.messages.HostConfig
 import com.spotify.docker.client.messages.PortBinding
+import com.spotify.docker.client.messages.Volume
 
 /**
  * Created by horse on 11/11/17.
@@ -9,10 +10,16 @@ import com.spotify.docker.client.messages.PortBinding
 
 fun main(args: Array<String>) {
     val docker = DefaultDockerClient.fromEnv().build()
+    val hostConfig: HostConfig = with(HostConfig.builder()){
+        appendBinds("/var/run/docker.sock:/var/run/docker.sock")
+        build()
+    }
+
     val containerConfig = ContainerConfig.builder().apply {
-        image("ubuntu")
+        image("4l4k4z4m`")
         tty(true)
         cmd("tail")
+        hostConfig(hostConfig)
     }.build()
 
     val container = docker.createContainer(containerConfig)
